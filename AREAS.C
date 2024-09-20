e886dac67dc8b625ecf8bf521e1d97c4d5be3146
vivek030roy <vivekanand020301@gmail.com>
2024-09-20 12:18:11 +0530

    Add files via upload

---
+++ AREAS.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a=10,r=20,b=10,l=15;
+printf("\nArea of square=%d",a*a);
+int ps;
+ps=4*a*a;
+printf("Perimeter of square=%d",ps);
+getch();
+}
---
+++ FLOATMUL.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+	{
+	clrscr();
+	float a=1.24,b=2.64,c;
+	c=a*b;
+	printf("multiplied product:%f",c);
+	printf("Multiplied product:%.1f",c);
+	getch();
+
+	}
